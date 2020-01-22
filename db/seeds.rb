6612c47081f03716ce3026e953e3eb0dc1a1a028
David Elì <davidelimg1@gmail.com>
2020-01-22 16:06:29 -0600

    Add testing and moc data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rails db:seed command (or created
+# alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#  Character.create(name: 'Luke', movie: movies.first)
+User.create!(username: 'Example User',
+             email: 'example@railstutorial.org',
+             password: 'foobar',
+             password_confirmation: 'foobar')
+
+9.times do |n|
+  username  = Faker::Name.name
+  email = "example-#{n + 1}@railstutorial.org"
+  password = 'password'
+  User.create!(username: username,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+users = User.all
+10.times do
+  users.each do |user|
+    user.posts.create(content: Faker::Lorem.sentence(15))
+  end
+end
