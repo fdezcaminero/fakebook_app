7b8da4edecd3abf62d51b164d7e1259ad65c4e80
Alisson Peres <alissonpres.dll@gmail.com>
2020-01-23 17:32:50 -0300

    Fixed links and controllers for like posts

--- app/controllers/like_posts_controller.rb
+++ app/controllers/like_posts_controller.rb
@@ -1,13 +1,14 @@
 class LikePostsController < ApplicationController
   before_action :find_post
-  before_action :find_like, only: [:destroy]
-  
+  before_action :find_like, only: :destroy
+
   def create
     if already_liked?
       flash[:notice] = "You can't like more than once"
     else
       @post.liked_posts.create(user: current_user)
     end
+    redirect_to posts_path
   end
 
   def destroy
@@ -26,7 +27,7 @@ class LikePostsController < ApplicationController
   end
 
   def find_like
-    @liked_post = @post.liked_posts(params[:id])
+    @liked_post = LikedPost.where(user_id: current_user.id, post_id: params[:post_id]).first
   end
 
   def already_liked?
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts do
-    resources :liked_posts, only: %i[create destroy]
+    resources :like_posts, only: %i[create destroy]
   end
   resources :users, only: %I[index show]
   # get 'users/index'
