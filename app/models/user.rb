f6ca5b0c588a5f20e2fe5d2f7d014b45bd519b71
David Elì <davidelimg1@gmail.com>
2020-01-27 15:09:13 -0600

    Fixing lint issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,6 +46,7 @@ class User < ApplicationRecord
   end
 
   def remove_friendship(friend_id)
-    following_friendships.find_by_requestee_id(friend_id)&.destroy || followers_friendships.find_by_requester_id(friend_id)&.destroy
+    following_friendships.find_by_requestee_id(friend_id)&.destroy ||
+    followers_friendships.find_by_requester_id(friend_id)&.destroy
   end
 end
