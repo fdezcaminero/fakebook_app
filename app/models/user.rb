f169b185af7084b1db9a55982e8643283af03526
Alisson Peres <alissonpres.dll@gmail.com>
2020-01-30 17:28:14 -0300

    updated omniauth settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,20 +49,26 @@ class User < ApplicationRecord
       followers_friendships.find_by_requester_id(friend_id)&.destroy
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.password_confirmation = user.password
+    end
+  end
+
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if (data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info'])
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(email: auth_hash['info']['email']).first_or_create do |u|
+      u.name = auth_hash['info']['name']
+      u.password = SecureRandom.hex
     end
   end
 end
